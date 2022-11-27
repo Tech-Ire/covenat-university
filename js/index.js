5123cca73da92db99c66b077587b02a4c9f29d0b
ajetunmobi ireoluwa <misiajet@gmail.com>
2022-11-26 15:20:26 -1200

    fixing js

--- js/index.js
+++ js/index.js
@@ -1,12 +1,13 @@
 var navlinks = document.getElementById("navlinks");
 
 function showMenu() {
+    navlinks.style.display = "block"
     navlinks.style.right = "0";
     console.log("Menu shown")
 }
 function hideMenu() {
     navlinks.style.right = "-200px";
-    navlinks.style.display = "none"
+    navlinks.style.display = "none";
     console.log("Menu closed")
 
 }
